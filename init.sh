1e2ec9448eb625bf3baa01bcc99d06c8dee4c613
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-02-25 19:38:21 -0300

    Basic setup

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo -v
+# Install and setup Homebrew
+if [[ -n $(command -v brew) ]]; then
+  echo "Brew already installed"
+else
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if [[ -z $(command -v brew) ]]; then
+  echo "Failed to install brew"
+  exit 1
+fi
+
+brew update
+brew bundle install
+brew upgrade
+# Install all brew applications from Brewfile backup
+
+
+
+
+
