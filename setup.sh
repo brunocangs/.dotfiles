bd08d7721e0ddda40d0de4117477abdff5549ddb
Bruno Cangussu <brunocangussu@Brunos-MacBook-Pro.local>
2024-05-12 11:36:37 -0300

    Fixes

--- setup.sh
+++ setup.sh
@@ -2,12 +2,14 @@
 
 sudo -v
 
-# Setup command line tools
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-softwareupdate -i -a
-rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+if [[ -z $(command -v git) ]]; then
+  # Setup command line tools
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  softwareupdate -i -a
+  rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+fi
 
-# Install and setup Homebrew
+Install and setup Homebrew
 if [[ -n $(command -v brew) ]]; then
   echo "Brew already installed"
 else
@@ -20,7 +22,7 @@ if [[ -z $(command -v brew) ]]; then
 fi
 
 # Setup fonts
-if [[ -z ls $HOME/Library/Fonts | grep -i power ]]; then
+if [[ -z $(ls "$HOME/Library/Fonts" | grep -i power) ]]; then
   echo "No powerline font found"
   # clone
   git clone https://github.com/powerline/fonts.git --depth=1
@@ -40,15 +42,15 @@ brew update;
 brew bundle install;
 brew upgrade;
 
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
+asdf plugin add nodejs;
 # Reads and parses node version list for latest lst                 From array return, find one with not lts = false an extract version. Then parse "vxx.xx.xx" into xx.xx.xx
 NODE_LTS=$(curl https://nodejs.org/download/release/index.json | jq '[.[] | select(.lts != false)] | .[0] | .version' | sed 's/"v\([^"]*\)"/\1/');
 asdf install nodejs $NODE_LTS && asdf global nodejs "$NODE_LTS";
 corepack enable;
 
 
-# Install all brew applications from Brewfile backup
-# Stow folders and apply all configs
+Install all brew applications from Brewfile backup
+Stow folders and apply all configs
 STOW_FOLDERS="nvim tmux zsh"
 for folder in $STOW_FOLDERS
 do
